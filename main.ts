f59a93595a666912e893c4e44f0abb3aa9e0da2f
DaisyMed27 <120490349+DaisyMed27@users.noreply.github.com>
2023-01-25 19:50:43 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
 function chooseHero () {
     // change sprite later to agree with Daisy 
     mySprite = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        f f f f f f c c f f f c . 
+        f f f c f f f f f f f c . 
+        c c c f f f e e f f c c . 
+        f f f 8 8 8 8 8 8 f c f . 
+        f f 8 b f 8 8 f b 8 f f . 
+        . f 8 1 f 8 8 f 1 8 f . . 
+        . f f 8 8 8 8 8 8 f f . . 
+        . f f f 4 4 4 4 f f f . . 
+        5 8 f 8 8 5 5 8 8 f 8 5 . 
+        8 5 f c 8 5 5 8 c f 5 8 . 
+        e e f c c 8 8 c c f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
         `, SpriteKind.Player)
     mySprite.setPosition(10, 59)
     while (mySprite.x < 75) {
